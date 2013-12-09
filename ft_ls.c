700aceebbd785843d56902949c1e1677682f4726
Georges-Michael PETROV <gpetrov@e1r12p4.42.fr>
2013-12-09 15:14:58 +0100

    ft_display created

--- ft_ls.c
+++ ft_ls.c
@@ -10,9 +10,10 @@
 
 #include "head.h"
 
+
 int		main(int argc, char **argv )
 {
-	ls_type		lst;
+//	ls_type		lst;
 	DIR			*dir;
 	struct dirent	*sd;
 	dir = opendir(".");
@@ -22,22 +23,30 @@ int		main(int argc, char **argv )
 		exit(1);
 	}
 	if (argc == 1)
-	{
-		while ((sd = readdir(dir)) != NULL)
-		{
-			if (sd->d_name[0] != '.')
-				ft_putendl(sd->d_name);
-		}
-	}
-	else
-		ft_ls_type(&lst, argc, **argv);
+		ft_display(sd, dir);
+	if (argv[1][0] == '-' && argv[1][1] == 'a')
+		ft_putstr("HELLO");
+	closedir(dir);
 	return (1);
 }
 
-/* the part that does ls -a
- * if (argv[1][0] == '-' && argv[1][1] == 'a')
+void	ft_display(struct dirent *sd, DIR *dir)
+{
+	while ((sd = readdir(dir)) != NULL)
 	{
-		while ((sd = readdir(dir)) != NULL)
-			ft_putendl(sd->d_name);
+		if (sd->d_name[0] != '.')
+		ft_putendl(sd->d_name);
 	}
+}
+/* the part that does ls -a
+ * void	ft_a(dirent *sd, char **argv)
+{
+if (argv[1][0] == '-' && argv[1][1] == 'a')
+	while ((sd = readdir(dir)) != NULL)
+			ft_putendl(sd->d_name);
+}
+
 	*/
+
+// else
+// 		ft_ls_type(&lst, argc, **argv);
