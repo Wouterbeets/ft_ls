eff62633e3aa25fc0396c9ac211743ff7f66e0fe
Wouter BEETS <wbeets@e1r13p7.42.fr>
2013-12-09 18:06:21 +0100

    oke

--- ft_ls.c
+++ ft_ls.c
@@ -13,7 +13,7 @@
 
 int		main(int argc, char **argv )
 {
-//	ls_type		lst;
+	ls_type		lst;
 	DIR			*dir;
 	struct dirent	*sd;
 	dir = opendir(".");
@@ -24,8 +24,13 @@ int		main(int argc, char **argv )
 	}
 	if (argc == 1)
 		ft_display(sd, dir);
-	if (argv[1][0] == '-' && argv[1][1] == 'a')
-		ft_a(sd, dir);
+	else
+		ft_ls_type(&lst, dir, argc, argv);
+	if (lst.error == 1)
+		ft_putendl("usage: asldfaskldjfh");
+
+//	if (argv[1][0] == '-' && argv[1][1] == 'a')
+//		ft_a(sd, dir);
 	closedir(dir);
 	return (1);
 }
@@ -35,7 +40,7 @@ void	ft_display(struct dirent *sd, DIR *dir)
 	while ((sd = readdir(dir)) != NULL)
 	{
 		if (sd->d_name[0] != '.')
-			ft_putendl(sd->d_name);		
+			ft_putendl(sd->d_name);
 	}
 }
 
